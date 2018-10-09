566fa44a2a2f14846cfdb8c1f80d542d9c46c866
Lokel123 <karol.wycisk@wp.pl>
2018-10-09 12:35:14 +0200

    ćwiczenie choinka

--- python/figury.py
+++ python/figury.py
@@ -10,27 +10,45 @@ def prostokat1(a, b, znak):
         print()
 
 
-def prostokat2(a, b, znak):
+def prostokat2(a, b, znak, znak2):
     
     for i in range(a):
         for j in range(b):
-            if j == 0 or j == b - 1
-             print(znak, end='')
+            if j == 0 or j == b - 1 or i == 0 or i == a - 1:
+                print(znak, end='')
             else:
-                print(" ", end='')
+                print(znak2, end='')
         print()
+
+
+def choinka(h, znak):
+    
+    for k in range(h):
+        if k == 0 or k == 
+    
+    
+    
+    
+def choinka2(h, znak):
+
+
 
 def main(args):
     
     a = int(input("Podaj 1 bok: "))
     b = int(input("Podaj 2 bok: "))
-    znak = input("Podaj znak: ")
-    
+    znak = input("Podaj 1 znak: ")
+    znak2 = input("Podaj 2 znak: ")
+    h = int(input("Podaj wysokość choinki: "))
     
     
     prostokat1(a, b, znak)
-    print(" ")
-    prostokat2(a, b, znak)
+    print()
+    prostokat2(a, b, znak, znak2)
+    print()
+    choinka(h, znak)
+    print()
+    choinka2(h, znak)
     
     
     return 0
