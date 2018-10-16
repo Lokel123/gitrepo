bdddcbb0647492d4a9bdf3d92cf3ec65ec80b69d
Lokel123 <karol.wycisk@wp.pl>
2018-10-16 12:35:53 +0200

    silnia i assert

--- python/potega.py
+++ python/potega.py
@@ -10,13 +10,20 @@ def potega_it(a, n):
     for i in range(n):
         
         wynik = wynik * a
-         print(wynik)
+        #print(wynik)
      
     return wynik
 
 def main(args):
-    a, n = 3, 4
-    potega_it("Potega {} do {} wynosi {}".format(a, n, potega_it(a, n)))
+    
+    #a = int(input("Podaj podstawę: "))
+    #n = int(input("Podaj wykładnik: "))
+    
+    #print("Potega {} do {} wynosi {}".format(a, n, potega_it(a, n)))
+    assert(potega_it(3 ,2) == 9)
+    assert(potega_it(3, 3) == 27)
+    assert(potega_it(0, 2) == 0)
+    
     return 0
 
 if __name__ == '__main__':
