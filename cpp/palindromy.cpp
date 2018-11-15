db9f8ff997f3b615bdef49e838ebd2f2cfdf2f1a
Lokel123 <karol.wycisk@wp.pl>
2018-11-15 07:56:03 +0100

    palindromy cz.2

--- cpp/palindromy.cpp
+++ cpp/palindromy.cpp
@@ -6,21 +6,31 @@
 #include <iostream>
 using namespace std;
 
-bool palindrom(w[], int r)
-
+bool palindrom(char w[], int r) {
+    bool czy_pal = true;
+    for (int i = 0; i < r / 2; i++) { 
+        
+        if (w[i] != w[r - 1 - i]) {
+            czy_pal = false;
+            break;
+        }
+        
+    }
+    return czy_pal;
+}
 
 int main(int argc, char **argv)
 {
 	const int rozmiar = 20;
-    char wyraz[20];
+    char wyraz[rozmiar];
     cout << "Podaj wyraz lub zdanie: ";
     cin.getline(wyraz, rozmiar);
     cout << cin.gcount() << endl;
     cout << strlen(wyraz) << endl;
     if (palindrom(wyraz, strlen(wyraz)))
-        cout << "Palinddrom!";
-    else
-        cout << "Nie Palinddrom";
+        cout << " Palindrom!";
+        else
+        cout << " Nie Palindrom";
     
 	return 0;
 }
