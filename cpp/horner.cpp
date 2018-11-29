0a2788c4fb9a1d903ad4b98f14a2d1fd9c696ab4
Lokel123 <karol.wycisk@wp.pl>
2018-11-29 07:55:19 +0100

    horner

---
+++ cpp/horner.cpp
@@ -0,0 +1,39 @@
+/*
+ * horner.cpp
+ *
+ * W(x) = 2x^3 + 3x^2 + 5x + 4
+ * W(x) = x(x( 2x + 3) + 5) + 4 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+void drukujw(int stopien, float tbwsp[]) {
+    
+    for (int i; i
+}
+
+int main(int argc, char **argv)
+{
+    
+    float *tbwsp; //wskaźnik
+    float x = 0;
+    int stopien = 0;
+    cout << "Podaj stopień wielomianu: ";
+    cin >> stopien;
+	tbwsp = new float [stopien = 1];
+    cout << tbwsp;
+    for (int 1 = 0; i <= stopien; i++) {
+        cout << "Podaj współczynnik przy potędze " << stopen - i << ":";
+        cin >> tbwsp[i];
+    
+    }
+    cout << "Podaj argument: ";
+    cinn >> x;
+    
+    cout << "Wartość wielomianu o postaci: "
+    drukujw
+	return 0;
+}
+
