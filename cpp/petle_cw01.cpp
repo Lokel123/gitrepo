54fecf1b65d8e50e193b83ec2b2091520d8a9086
Lokel123 <karol.wycisk@wp.pl>
2018-12-20 07:56:25 +0100

    ćwiczenia2

---
+++ cpp/petle_cw01.cpp
@@ -0,0 +1,37 @@
+/*
+ * petle_cw01.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+void cw01() {
+    int i;
+    int suma;
+    for (i = 10; i < 100; i ++) {
+        if (i % 2 == 0)
+        suma = suma + i;
+    }
+        cout << suma << endl;
+}
+
+void cw02() {
+    int l1;
+    int l2;
+    int l3;
+    cout << "Podaj liczbę: ";
+    cin >> l1;
+    cout << "Podaj drugą liczbę: ";
+    cin >> l2;
+    cout << "Podaj trzecią liczbę: ";
+    cin >> l3;
+    while (l1+l2==l3
+}
+
+int main(int argc, char **argv)
+{
+	cw01();
+	return 0;
+}
+
