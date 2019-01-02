47daee691fe31f0e6110fab32a55b8ff8d1fd65e
Lokel123 <karol.wycisk@wp.pl>
2019-01-02 08:45:35 +0100

    sortowanie cz2

--- cpp/sortowanie.cpp
+++ cpp/sortowanie.cpp
@@ -21,33 +21,48 @@ void drukuj(int tab[], int rozmiar){
             }
 }
 
-void zamien1(int a, int b) {
-    cout << a << " " << b << endl;
+void zamien1(int &a, int &b) {
+    //cout << a << " " << b << endl;
     int tmp = a;
     a = b;
     b = tmp;
-    cout << a << " " << b << endl;
+    //cout << a << " " << b << endl;
 }
 
 void sort_bubble(int tab[], int n) {
     for (int j = n-1; j > 0; j--) {
+        
         for (int i = 0; i < j, i++) {
             if (tab[i] > tab[i+1])
                 zamien1(tab[i], tab[i+1]);
         }
     }    
 }
-
+void sort_selection(int tab[],int n){
+    int i, j, k;
+    for (i = 0; i < n, i++) {
+        k = i; //indeks najmniejszego elementu
+        for (j = i+1,j < n,j++){
+        if (tab[j] < tab[k])
+            k = j;
+        }
+        zamien1(tab[i], tab[k])
+    }
+    
+}
 int main(int argc, char **argv)
 {
 
     int roz = 20;
     int tab[roz];
     wypelnij(tab, roz);
-    cout << endl;
-    //sort_bubble(tab, roz);
-    zamien1(5, 10);
     drukuj(tab, roz);
+    cout << endl << endl;
+    //sort_bubble(tab, roz);
+    sort_selection(tab, roz);
+    drukuj(tab, roz);
+    zamien1(5, 10);
+    cout << endl;
     
 	return 0;
 }
