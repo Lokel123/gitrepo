552071ac34358baa7ef387a433a4c724ba3ec38a
Lokel123 <karol.wycisk@wp.pl>
2019-02-26 12:36:06 +0100

    szyfr

--- cpp/szyfr_przestawieniowy.cpp
+++ cpp/szyfr_przestawieniowy.cpp
@@ -9,16 +9,38 @@ using namespace std;
 
 #define MAKS 100
 
+
+void drukuj(char tb[]){
+        
+    }
+}
+
 void szyfruj(char tb[], int klucz) {
     int ile = strlen(tb);
     int reszta;
-    cout << ile <<endl;
+    int i = 0;
+    cout << ile << endl;
     reszta = ile % klucz;
+    if (reszta > 0)
+        for(i = 0; i < klucz-reszta; i++) {
+            tb[ile+i] = '.';
+        }
+    tb[ile+i]= '\0';
+    drukuj(tb);
+    ile = strlen(tb);
+    cout << endl;
+    for(i= 0; i < kulcz; i++) {
+        for(int j=i; j<ile; j+=klucz) {
+            cout << tb[j];
+        }
+    }
+}
+    drukuj(tab);
+    while(tb[i] != '\0') {
+        cout << tb[i];
+        i++;
+    }
     cout << reszta << endl;
-    
-    
-    
-    
 }
 
 int main(int argc, char **argv)
